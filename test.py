5959764171680c13354578eb077bd8240648f7f6
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-29 01:44:00 -0700

    added divide function

--- test.py
+++ test.py
@@ -13,6 +13,9 @@ class TestCalculator(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual(calculator.multiply(4, 4), 16)
 
+    def test_divide(self):
+        self.assertEqual(calculator.divide(5, 5), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
