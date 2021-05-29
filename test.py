f1581a62933777570cd1c9a450c240ee02d287cf
BobbySinclusto <allencbenjamin@gmail.com>
2021-05-29 01:38:45 -0700

    fixed lilnting error

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(5, calculator.add(3, 2))
-    
+
     def test_subtract(self):
         self.assertEqual(calculator.subtract(1, 1), 0)
 
